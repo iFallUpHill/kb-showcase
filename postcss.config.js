5db5f64775a7a29c9ce534fcb4e2a62bc955bb51
Jason Manson-Hing <jason.mansonhing@gmail.com>
2020-02-18 02:56:41 -0500

    swap uncss for purgecss and import boba

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
-    require('postcss-uncss')({
-      html: ['./src/*.html']
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./src/**/*.html']
     })
   ]
 }
