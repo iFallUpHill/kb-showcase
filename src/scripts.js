ddbe7be846647e822d46b1bc24589c5506524253
Jason Manson-Hing <jason.mansonhing@gmail.com>
2020-04-03 04:35:21 -0400

    started working on lazy-loading images

---
+++ src/scripts.js
@@ -0,0 +1,6 @@
+import "../node_modules/lazysizes/plugins/object-fit/ls.object-fit.js";
+import "../node_modules/lazysizes/plugins/parent-fit/ls.parent-fit.js";
+import "../node_modules/lazysizes/plugins/blur-up/ls.blur-up.js";
+import "../node_modules/lazysizes/lazysizes.js";
+
+lazysizes.init();
