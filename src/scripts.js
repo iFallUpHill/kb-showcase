4328d75c24775d3d0f9364372f5b3e8812fee366
Jason Manson-Hing <jason.mansonhing@gmail.com>
2020-04-03 21:51:14 -0400

    fixed lazysizes and removed postcss purge

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,3 @@
-import "../node_modules/lazysizes/plugins/object-fit/ls.object-fit.js";
-import "../node_modules/lazysizes/plugins/parent-fit/ls.parent-fit.js";
 import "../node_modules/lazysizes/plugins/blur-up/ls.blur-up.js";
 import "../node_modules/lazysizes/lazysizes.js";
 
