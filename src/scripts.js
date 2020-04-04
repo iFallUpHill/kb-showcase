2d233a7c81028b100aabcb571c0be4890c176db1
Jason Manson-Hing <jason.mansonhing@gmail.com>
2020-04-03 23:43:32 -0400

    updated images and fixed buttons

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,7 @@
-import "../node_modules/lazysizes/plugins/blur-up/ls.blur-up.js";
-import "../node_modules/lazysizes/lazysizes.js";
+// import "../node_modules/lazysizes/lazysizes.js";
+// import "../node_modules/lazysizes/plugins/blur-up/ls.blur-up.js";
 
-lazysizes.init();
+import lazySizes from 'lazysizes';
+import 'lazysizes/plugins/blur-up/ls.blur-up';
+
+lazySizes.init();
